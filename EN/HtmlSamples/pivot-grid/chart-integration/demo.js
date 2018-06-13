8b27de712935b023a6240c694d8b79677950a054
Flaming Watermelons <flamingwatermelons@example.com>
2018-06-13 05:32:09 -0400

    SB build 18.1.42

--- EN/HtmlSamples/pivot-grid/chart-integration/demo.js
+++ EN/HtmlSamples/pivot-grid/chart-integration/demo.js
@@ -154,7 +154,7 @@ $(function () {
                         horizontalZoomable: true,
                         verticalZoomable: true,
                         windowResponse: "immediate",
-                        title: "$$(RevenuePerUnitsSold)"
+                        title: "Revenue per Units Sold"
                     });
                 };
 
